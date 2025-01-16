badc9b12e2214aec113dd8ba1b219ae3ab58d1a2
Javi <Jjaviergonzalez23@gmail.com>
2025-01-16 11:49:06 +0100

    'tic-tac-toe'

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Square } from './components/Square'
 import confetti from 'canvas-confetti';
-
-const TURNS = {
-  X: '❌',
-  O: '⚪'
-}
-
-const WINNER_COMBS =[
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+import {TURNS, WINNER_COMBS} from './data/data'
 
 function App() {
 
